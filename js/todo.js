2be50542b4cacf58f26bd55015efa7d52c2204b0
Marc-Anthony17 <102612134+Marc-Anthony17@users.noreply.github.com>
2024-03-04 20:35:14 +0200

    completed the js render

--- js/todo.js
+++ js/todo.js
@@ -1,36 +1,62 @@
-// todo.js
+// Function to render the ToDo list
+function renderTodos(todos) {
+    const todoList = document.getElementById('todo-list');
+    todoList.innerHTML = '';
 
-// Sample ToDo data
-let todos = [
-    { text: 'Learn HTML', completed: false },
-    { text: 'Learn CSS', completed: true },
-    { text: 'Learn JavaScript', completed: false }
-];
+    todos.forEach((todo, index) => {
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todo-item');
 
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = todo.completed;
+        checkbox.addEventListener('change', () => toggleTodoCompleted(todos, index));
+
+        const todoText = document.createElement('span');
+        todoText.textContent = todo.text;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => deleteTodo(todos,index));
+
+        todoItem.appendChild(checkbox);
+        todoItem.appendChild(todoText);
+        todoItem.appendChild(deleteButton);
+
+        todoList.appendChild(todoItem);
+    });
+}
 
 // Function to add a new ToDo
-function addTodo(todoList, text) {
+function addTodo(todos) {
+    const newTodoInput = document.getElementById('new-todo');
+    const newTodoText = newTodoInput.value.trim();
 
-    todoList.push({ text: text, completed: false });
-       
+    if (newTodoText) {
+        todos.push({ text: newTodoText, completed: false });
+        newTodoInput.value = '';
+        renderTodos(todos);
+    }
 }
 
 // Function to toggle the completed status of a ToDo
-function toggleTodoCompleted(todoList,index) {
-    todoList[index].completed = !todoList[index].completed;
+function toggleTodoCompleted(todos,index) {
+    todos[index].completed = !todos[index].completed;
+    renderTodos(todos);
 }
 
 // Function to delete a ToDo
-function deleteTodo(todoList,index) {
-    todoList.splice(index, 1);
+function deleteTodo(todos,index) {
+    todos.splice(index, 1);
+    renderTodos(todos);
 }
 
 // Export functions for testing
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = {
+        renderTodos,
         addTodo,
         toggleTodoCompleted,
         deleteTodo,
-        todos,
     };
 }
